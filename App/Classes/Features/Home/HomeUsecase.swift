c9ac7475f813affc5471100232f17e24a9cb7dcd
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-06-11 18:17:00 +0700

    Usecase refactoring

--- App/Classes/Features/Home/HomeUsecase.swift
+++ App/Classes/Features/Home/HomeUsecase.swift
@@ -1,17 +1,27 @@
 import RxSwift
 
-class HomeUsecase: Usecase<HomeEntity, HomeParam> {
+class HomeUsecase: Usecase {
 
     private let repository: HomeRepository
+    private let executor: JobExecutor
+    private let thread: UIThread
 
     init(repository: HomeRepository,
          executor: JobExecutor,
          thread: UIThread) {
         self.repository = repository
-        super.init(jobExecutor: executor, uiThread: thread)
+        self.executor = executor
+        self.thread = thread
     }
 
-    override func buildUsecaseObservable(params: HomeParam) -> PrimitiveSequence<SingleTrait, HomeEntity> {
+    var jobExecutor: JobExecutor { return executor }
+    var uiThread: UIThread { return thread }
+    var disposables: CompositeDisposable { return CompositeDisposable() }
+}
+
+extension HomeUsecase {
+
+    func buildUsecaseObservable(params: HomeParam) -> Single<HomeEntity> {
         return repository.discoverMovie(withParam: params)
     }
 }
